082972e163c68e88ca6ca9e7054659408fc277b6
sctech <106792792+sctech5@users.noreply.github.com>
2024-02-25 09:41:22 +0700

    Update qosmo.js

--- res/qosmo.js
+++ res/qosmo.js
@@ -3,6 +3,8 @@ m = {}
 addEventListener('load', () => {
 	
 	m.ct = el({a:'div', b:document.body})
+	m.page4 = page4(m.ct);
+	m.page3 = page3(m.ct);
 	m.page2 = page2(m.ct);
 	m.page1 = page1(m.ct);
 	
@@ -10,7 +12,7 @@ addEventListener('load', () => {
 	(a => {
 		
 		(a => {
-			for (var i=1; i<6; i++) el({a:'option', b:a, c:`Page${i}`, d:{value:`page${i}`} })
+			for (var i=1; i<5; i++) el({a:'option', b:a, c:`Page${i}`, d:{value:`page${i}`} })
 		})(el({a:'select', b:a, e:{change:a => { m.ct.innerHTML = ''; m.ct.appendChild(m[a.target.value]) }} }))
 		
 		el({a:'div', b:a, c:'Qosmo Dashboard'})
